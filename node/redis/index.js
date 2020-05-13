5375c0bb78d5507312896bf4dba8f9569c907182
Stephaie Schustermann <schustermann21@gmail.com>
2020-05-13 12:18:53 +0300

    zrange

--- node/redis/index.js
+++ node/redis/index.js
@@ -4,7 +4,7 @@ const express = require('express'),
 
 var app = express();
 //setup redis data
-redis.client.flushall(); // clean redis
+redis.client.flushall();
 redis.client.hmset('dog:1', ['name', 'gizmo', 'age', '5']);
 redis.client.hmset('dog:2', ['name', 'dexter', 'age', '6']);
 redis.client.hmset('dog:3', ['name', 'fido', 'age', '5']);
@@ -12,9 +12,6 @@ redis.client.hmset('dog:3', ['name', 'fido', 'age', '5']);
 redis.client.set('dog:name:gizmo', 'dog:1');
 redis.client.set('dog:name:dexter', 'dog:2');
 redis.client.set('dog:name:fido', 'dog:3');
-//ages are not unique
-redis.client.lpush('dog:age:5', ['dog:1', 'dog:3']);
-redis.client.lpush('dog:age:6', 'dog:2');
 
 app.use((req, res, next) => {
   console.time('request');
@@ -22,15 +19,32 @@ app.use((req, res, next) => {
 });
 
 app.get('/dog/name/:name', (req, res) => {
+  var now = Date.now();
+  redis.client.zadd('dog:last-lookup', [now, 'dog:name:' + req.params.name]);
   //first find the id
   redis.get('dog:name:' + req.params.name)
+  .then((data) => {
+    redis.client.hset(data, 'last-lookup', now);
+    return data;
+  })
   .then(redis.hgetall)
   .then((data) => res.send(data));
   console.timeEnd('request');
 });
 
-app.get('/dog/age/:age', (req, res) => {
-  redis.lrange('dog:age:' + req.params.age)
+app.get('/dog/any', (req, res) => {
+  redis.zrevrangebyscore('dog:last-lookup', '+inf', '-inf')
+  .then((data) => Promise.all(data.map(redis.get)))
+  .then((data) => Promise.all(data.map(redis.hgetall)))
+  .then((data) => res.send(data));
+  console.timeEnd('request');
+});
+
+app.get('/dog/latest', (req, res) => {
+  var now = Date.now();
+  var minuteAgo = now - 60000;
+  redis.zrevrangebyscore('dog:last-lookup', now, minuteAgo)
+  .then((data) => Promise.all(data.map(redis.get)))
   .then((data) => Promise.all(data.map(redis.hgetall)))
   .then((data) => res.send(data));
   console.timeEnd('request');
--- node/redis/redis.js
+++ node/redis/redis.js
@@ -3,35 +3,33 @@ redis = require('redis');
 
 var client = redis.createClient(config.redis_port, config.redis_host);
 
+var promiser = (resolve, reject) => {
+  return (err, data) => {
+    if(err) reject(err);
+    resolve(data);
+  };
+};
+
 var get = (key) => {
   return new Promise((resolve, reject) => {
-    client.get(key, (err, data) => {
-      if(err) reject(err);
-      resolve(data);
-    });
+    client.get(key, promiser(resolve, reject));
   });
 };
 
 var hgetall = (key) => {
   return new Promise((resolve, reject) => {
     if(key === null) reject();
-    client.hgetall(key, (err, data) => {
-      if(err) reject(err);
-      resolve(data);
-    });
+    client.hgetall(key, promiser(resolve, reject));
   });
 };
 
-var lrange = (key) => {
+var zrevrangebyscore = (key, max, min) => {
   return new Promise((resolve, reject) => {
-    client.lrange(key, [0, -1], (err, data) => {
-      if(err) reject(err);
-      resolve(data);
-    });
+    client.zrevrangebyscore(key, max, min, promiser(resolve, reject));
   });
 };
 
 module.exports.get = get;
 module.exports.hgetall = hgetall;
-module.exports.lrange = lrange;
+module.exports.zrevrangebyscore = zrevrangebyscore;
 module.exports.client = client;

40dca0b8cc1751a06de691df51e1c92821c3f4df
Stephaie Schustermann <schustermann21@gmail.com>
2020-05-13 11:49:18 +0300

    dogs

--- node/redis/index.js
+++ node/redis/index.js
@@ -1,22 +1,39 @@
 const express = require('express'),
-  config = require('./config.js'),
   process = require('process'),
-  redis = require('redis');
+  redis = require('./redis.js');
 
 var app = express();
-//connect to redis
-var redisClient = redis.createClient(config.redis_port, config.redis_host);
-var publishClient = redis.createClient(config.redis_port, config.redis_host);
-redisClient.on("message", (channel, message) => {
-  console.log(message);
+//setup redis data
+redis.client.flushall(); // clean redis
+redis.client.hmset('dog:1', ['name', 'gizmo', 'age', '5']);
+redis.client.hmset('dog:2', ['name', 'dexter', 'age', '6']);
+redis.client.hmset('dog:3', ['name', 'fido', 'age', '5']);
+//we are using name like username, unique
+redis.client.set('dog:name:gizmo', 'dog:1');
+redis.client.set('dog:name:dexter', 'dog:2');
+redis.client.set('dog:name:fido', 'dog:3');
+//ages are not unique
+redis.client.lpush('dog:age:5', ['dog:1', 'dog:3']);
+redis.client.lpush('dog:age:6', 'dog:2');
+
+app.use((req, res, next) => {
+  console.time('request');
+  next();
 });
 
-redisClient.subscribe('REQUESTS');
+app.get('/dog/name/:name', (req, res) => {
+  //first find the id
+  redis.get('dog:name:' + req.params.name)
+  .then(redis.hgetall)
+  .then((data) => res.send(data));
+  console.timeEnd('request');
+});
 
-app.get('/', (req, res) => {
-  publishClient.publish('REQUESTS', `Request on ${req.socket.localPort} for ${req.url}`);
-  console.log(`Local log for ${req.url}`);
-  res.end();
+app.get('/dog/age/:age', (req, res) => {
+  redis.lrange('dog:age:' + req.params.age)
+  .then((data) => Promise.all(data.map(redis.hgetall)))
+  .then((data) => res.send(data));
+  console.timeEnd('request');
 });
 
 app.listen(process.argv[2]);

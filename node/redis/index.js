8720dc024c8b8a2665388f4ebc083e895a92331b
Stephaie Schustermann <schustermann21@gmail.com>
2020-05-13 11:36:24 +0300

    subcribe

--- node/redis/index.js
+++ node/redis/index.js
@@ -4,21 +4,19 @@ const express = require('express'),
   redis = require('redis');
 
 var app = express();
-
 //connect to redis
 var redisClient = redis.createClient(config.redis_port, config.redis_host);
-//redisClient.set('REDIS_KEY', '0');
-
-app.get('/', (req, res) => {
-  redisClient.incr('REDIS_KEY');
-  redisClient.get('REDIS_KEY', (err, reply) => {
-    res.send("<html><head><title>Page" +
-  "</title><head><body><h1>Our Redis and Express Web Application</h1>" +
-  "Redis count: " + reply + "</body></html>");
-    res.end();
-  });
-
+var publishClient = redis.createClient(config.redis_port, config.redis_host);
+redisClient.on("message", (channel, message) => {
+  console.log(message);
 });
 
-//app.listen(8080);
+redisClient.subscribe('REQUESTS');
+
+app.get('/', (req, res) => {
+  publishClient.publish('REQUESTS', `Request on ${req.socket.localPort} for ${req.url}`);
+  console.log(`Local log for ${req.url}`);
+  res.end();
+});
+
 app.listen(process.argv[2]);
